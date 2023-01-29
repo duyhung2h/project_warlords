1d2c7ab7f75029645270073d4624003469529439
duyhung2h <duyhung2h@gmail.com>
2023-01-29 15:59:40 +0700

    append season mechanic

--- Warlord - SeasonChange.py
+++ Warlord - SeasonChange.py
@@ -6,12 +6,13 @@ from AoE2ScenarioParser.scenarios.aoe2_de_scenario import AoE2DEScenario
 from functions.RebuildingTriggers import RebuildingTriggers
 from functions.indexing_file.indexing_file import IndexingFile
 # File & Folder setup
+from model.seasonal_bushes import SeasonalBushes
 from model.seasonal_trees import SeasonalTrees
 
 scenario_folder = "C:/Users/Admin/Games/Age of Empires 2 DE/76561198148041091/resources/_common/scenario/"
 
 # Source scenario to work with
-scenario_name = "12warlords 0v1v27"
+scenario_name = "12warlords 0v1v29"
 input_path = scenario_folder + scenario_name + ".aoe2scenario"
 output_path = scenario_folder + scenario_name + " modify Seasons" + ".aoe2scenario"
 
@@ -48,7 +49,45 @@ originalTerrains = source_scenario.map_manager.terrain
 map_manager = source_scenario.map_manager
 
 '''
+Define season transition time
+'''
+
+
+'''
+Transform trees to correct seasonal variant
+'''
+print("Transform trees to correct seasonal variant")
+trees_in_map_list = []
+bushes_in_map_list = []
+seasonal_trees_list = SeasonalTrees.get_seasonal_trees()
+seasonal_bushes_list = SeasonalBushes.get_seasonal_bushes()
+
+# Add the tree and bushes in the map to the seasonal list by chance
+for unit in unit_manager.get_player_units(player=0):
+    for seasonal_tree in seasonal_trees_list:
+        # add tree oak to list with chances based on elevation
+        if unit.rotation == seasonal_tree.SummerVariant and (
+                unit.unit_const == 349 or unit.unit_const == 411):  # Oak, Oak Forest
+            tile_below_unit = map_manager.get_tile(x=int(unit.x), y=int(unit.y))
+            randomChance = ((random.random() - 0.4) ** (1 - (tile_below_unit.elevation - 2) / 5)).real
+            if float(randomChance) > float(0.40):  # chances depends on elevation bro
+                print(unit)
+                trees_in_map_list.append(unit)
+for unit in unit_manager.get_player_units(player=0):
+    for seasonal_bush in seasonal_bushes_list:
+        # add bushes to list with chances based on elevation
+        if unit.rotation == seasonal_bush.SummerVariant and (
+                unit.unit_const == 1053):  # Bush B
+            tile_below_unit = map_manager.get_tile(x=int(unit.x), y=int(unit.y))
+            randomChance = ((random.random() - 0.4) ** (1 - (tile_below_unit.elevation - 2) / 5)).real
+            if float(randomChance) > float(0.40):  # chances depends on elevation bro
+                print(unit)
+                bushes_in_map_list.append(unit)
+
+'''
 create triggers that changes season manually (create ice patches and replace trees)
+
+de-transition trigger should also be inside map-size loop
 '''
 trigg_winter = source_trigger_manager.add_trigger(
     name="ModifySeasonWinter",
@@ -68,42 +107,6 @@ trigg_winter.new_effect.change_color_mood(
     quantity=10,
     color_mood=ColorMood.WINTER
 )
-trigg_winter_snow_transition_1 = source_trigger_manager.add_trigger(
-    name="ModifySeasonWinter_ST1",
-    enabled=False,
-    looping=False
-)
-trigg_winter_snow_transition_2 = source_trigger_manager.add_trigger(
-    name="ModifySeasonWinter_ST2",
-    enabled=False,
-    looping=False
-)
-trigg_winter_snow_transition_3 = source_trigger_manager.add_trigger(
-    name="ModifySeasonWinter_ST3",
-    enabled=False,
-    looping=False
-)
-trigg_winter_snow_transition_4 = source_trigger_manager.add_trigger(
-    name="ModifySeasonWinter_ST4",
-    enabled=False,
-    looping=False
-)
-trigg_winter_snow_transition_5 = source_trigger_manager.add_trigger(
-    name="ModifySeasonWinter_ST5",
-    enabled=False,
-    looping=False
-)
-trigg_winter_snow_transition_6 = source_trigger_manager.add_trigger(
-    name="ModifySeasonWinter_ST6",
-    enabled=False,
-    looping=False
-)
-trigg_winter_snow_transition_1.new_condition.timer(timer=5)
-trigg_winter_snow_transition_2.new_condition.timer(timer=5)
-trigg_winter_snow_transition_3.new_condition.timer(timer=5)
-trigg_winter_snow_transition_4.new_condition.timer(timer=5)
-trigg_winter_snow_transition_5.new_condition.timer(timer=5)
-trigg_winter_snow_transition_6.new_condition.timer(timer=5)
 # Allowed terrain (ALL LANDS TERRAIN)
 allowed_terrains = [0, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 24, 25, 26, 27, 29, 30, 31, 32, 36,
                     40, 41, 42, 45, 46, 48, 49, 50, 56, 60, 63, 64, 65, 66, 67, 70, 71, 72, 75, 76, 77, 78, 83, 88, 89,
@@ -113,6 +116,7 @@ random_ice_facets = [1, 2, 3, 4, 5, 6]
 
 '''
 random chances to create ice patch for every tiles
+Also change tree seasonal variant
 
 random chances would be higher depends on elevation:
 if it's 0 or 1 or 2, 0%
@@ -126,162 +130,133 @@ wolfram alpha:
 plot (x-0.4)^(1 - (<ele>-2)/5))
 8% -> 17.5% -> 32.5% -> 52.5% -> 100%
 '''
-for terrain in originalTerrains:
-    randomChance = ((random.random() - 0.4) ** (1 - (terrain.elevation - 2) / 5)).real
-    # print("X=" + str(terrain.x) + " Y=" + str(terrain.y) + " Elevation=" + str(
-    #     terrain.elevation) + " Random chance=" + str(
-    #     randomChance))
-    if float(randomChance) > float(0.6):  # chances depends on elevation bro
-        print("PASS!")
-        for allowed_terrain in allowed_terrains:
-            if terrain.terrain_id == allowed_terrain:
-                random_ice_facet = random.choice(random_ice_facets)
-                if random.random() > randomChance:  # 1/6
-                    trigg_winter_snow_transition_1.new_effect.create_object(
-                        object_list_unit_id=728,  # Ice, navigable
-                        source_player=0,
-                        location_x=terrain.x,
-                        location_y=terrain.y,
-                        facet=random_ice_facet
-                    )
-                    snow_tiles_log_file.write(
-                        "at x=" + str(terrain.x) + "; y=" + str(terrain.y) + ";facet=" + str(random_ice_facet) + "\n")
-                    if terrain.elevation < 6:
-                        continue
-                    random_ice_facet = random.choice(random_ice_facets)
-                if random.random() > randomChance:  # 1/6
-                    trigg_winter_snow_transition_2.new_effect.create_object(
-                        object_list_unit_id=728,  # Ice, navigable
-                        source_player=0,
-                        location_x=terrain.x,
-                        location_y=terrain.y,
-                        facet=random_ice_facet
-                    )
-                    snow_tiles_log_file.write(
-                        "at x=" + str(terrain.x) + "; y=" + str(terrain.y) + ";facet=" + str(random_ice_facet) + "\n")
-                    if terrain.elevation < 6:
-                        continue
-                    random_ice_facet = random.choice(random_ice_facets)
-                if random.random() > randomChance:  # 1/6
-                    trigg_winter_snow_transition_3.new_effect.create_object(
-                        object_list_unit_id=728,  # Ice, navigable
-                        source_player=0,
-                        location_x=terrain.x,
-                        location_y=terrain.y,
-                        facet=random_ice_facet
-                    )
-                    snow_tiles_log_file.write(
-                        "at x=" + str(terrain.x) + "; y=" + str(terrain.y) + ";facet=" + str(random_ice_facet) + "\n")
-                    if terrain.elevation < 6:
-                        continue
-                    random_ice_facet = random.choice(random_ice_facets)
-                if random.random() > randomChance:  # 1/6
-                    trigg_winter_snow_transition_4.new_effect.create_object(
-                        object_list_unit_id=728,  # Ice, navigable
-                        source_player=0,
-                        location_x=terrain.x,
-                        location_y=terrain.y,
-                        facet=random_ice_facet
-                    )
-                    snow_tiles_log_file.write(
-                        "at x=" + str(terrain.x) + "; y=" + str(terrain.y) + ";facet=" + str(random_ice_facet) + "\n")
-                    if terrain.elevation < 6:
-                        continue
-                    random_ice_facet = random.choice(random_ice_facets)
-                if random.random() > randomChance:  # 1/6
-                    trigg_winter_snow_transition_5.new_effect.create_object(
-                        object_list_unit_id=728,  # Ice, navigable
-                        source_player=0,
-                        location_x=terrain.x,
-                        location_y=terrain.y,
-                        facet=random_ice_facet
-                    )
-                    snow_tiles_log_file.writelines(
-                        "at x=" + str(terrain.x) + "; y=" + str(terrain.y) + ";facet=" + str(random_ice_facet) + "\n")
-                    if terrain.elevation < 6:
-                        continue
-                    random_ice_facet = random.choice(random_ice_facets)
-                if random.random() > randomChance:  # 1/6
-                    trigg_winter_snow_transition_6.new_effect.create_object(
-                        object_list_unit_id=728,  # Ice, navigable
-                        source_player=0,
-                        location_x=terrain.x,
-                        location_y=terrain.y,
-                        facet=random_ice_facet
-                    )
-                    snow_tiles_log_file.write(
-                        "at x=" + str(terrain.x) + "; y=" + str(terrain.y) + ";facet=" + str(random_ice_facet) + "\n")
-                    if terrain.elevation < 6:
-                        continue
-                    random_ice_facet = random.choice(random_ice_facets)
-trigg_winter.new_effect.activate_trigger(trigger_id=trigg_winter_snow_transition_1.trigger_id)
-trigg_winter_snow_transition_1.new_effect.activate_trigger(trigger_id=trigg_winter_snow_transition_2.trigger_id)
-trigg_winter_snow_transition_2.new_effect.activate_trigger(trigger_id=trigg_winter_snow_transition_3.trigger_id)
-trigg_winter_snow_transition_3.new_effect.activate_trigger(trigger_id=trigg_winter_snow_transition_4.trigger_id)
-trigg_winter_snow_transition_4.new_effect.activate_trigger(trigger_id=trigg_winter_snow_transition_5.trigger_id)
-trigg_winter_snow_transition_5.new_effect.activate_trigger(trigger_id=trigg_winter_snow_transition_6.trigger_id)
-
-
-'''
-Transform trees to correct seasonal variant
-'''
-print("Transform trees to correct seasonal variant")
-trees_in_map_list = []
-seasonal_trees_list = SeasonalTrees.get_seasonal_trees()
-for unit in unit_manager.get_player_units(player=0):
-    for seasonal_tree in seasonal_trees_list:
-        if unit.rotation == seasonal_tree.SummerVariant and (
-                unit.unit_const == 349 or unit.unit_const == 411):  # Oak, Oak Forest
-            tile_below_unit = map_manager.get_tile(x=int(unit.x), y=int(unit.y))
-            randomChance = ((random.random() - 0.4) ** (1 - (tile_below_unit.elevation - 2) / 5)).real
-            if float(randomChance) > float(0.40):  # chances depends on elevation bro
-                print(unit)
-                trees_in_map_list.append(unit)
-# winter change
-trigg_winter_tree_transition_disable = source_trigger_manager.add_trigger(
-    name="WinterTreeTransition_disable",
-    enabled=False,
-    looping=False
-)
-trigg_winter_snow_transition_5.new_effect.deactivate_trigger(trigger_id=trigg_winter_tree_transition_disable.trigger_id)
-for unit in trees_in_map_list:
-    trigg_winter_tree_transition = source_trigger_manager.add_trigger(
-        name="WinterTreeTransition_x=" + str(unit.x) + "y=" + str(unit.y),
+old_trigg_winter_snow_transition_y = trigg_winter
+for y in range(0, map_size + 1, 1):
+    trigg_winter_snow_transition_y = source_trigger_manager.add_trigger(
+        name="ModifySeasonWinter_ST" + str(y),
         enabled=False,
         looping=False
     )
-    trigg_winter.new_effect.activate_trigger(trigger_id=trigg_winter_tree_transition.trigger_id)
-    for seasonal_tree in seasonal_trees_list:
-        if unit.rotation == seasonal_tree.SummerVariant:
-            trigg_winter_tree_transition_disable.new_effect.deactivate_trigger(
-                trigg_winter_tree_transition_disable.trigger_id)
-            trigg_winter_tree_transition.new_condition.objects_in_area(
-                area_x1=int(unit.x),
-                area_x2=int(unit.x),
-                area_y1=int(unit.y),
-                area_y2=int(unit.y),
-                source_player=0,
-                quantity=1,
-                object_group=ObjectClass.TREE,
-                object_state=ObjectState.ALIVE
+    # last wave activating current wave
+    if 0 < y < map_size:
+        old_trigg_winter_snow_transition_y.new_effect.activate_trigger(
+            trigger_id=trigg_winter_snow_transition_y.trigger_id)
+    if y == 0:
+        trigg_winter.new_effect.activate_trigger(trigger_id=trigg_winter_snow_transition_y.trigger_id)
+    trigg_winter_snow_transition_y.new_condition.timer(timer=2)
+    for terrain in originalTerrains:
+        if terrain.y == y:
+            randomChance = ((random.random() - 0.4) ** (1 - (terrain.elevation - 2) / 5)).real
+            # print("X=" + str(terrain.x) + " Y=" + str(terrain.y) + " Elevation=" + str(
+            #     terrain.elevation) + " Random chance=" + str(
+            #     randomChance))
+            if float(randomChance) > float(0.6):  # chances depends on elevation bro
+                # print("PASS!")
+                for allowed_terrain in allowed_terrains:
+                    if terrain.terrain_id == allowed_terrain:
+                        random_ice_facet = random.choice(random_ice_facets)
+                        trigg_winter_snow_transition_y.new_effect.create_object(
+                            object_list_unit_id=728,  # Ice, navigable
+                            source_player=0,
+                            location_x=terrain.x,
+                            location_y=terrain.y,
+                            facet=random_ice_facet
+                        )
+                        snow_tiles_log_file.write(
+                            "at x=" + str(terrain.x) + "; y=" + str(terrain.y) + ";facet=" + str(
+                                random_ice_facet) + "\n")
+                        if terrain.elevation < 6:
+                            continue
+                        random_ice_facet = random.choice(random_ice_facets)
+        elif terrain.y > y:
+            break
+    # Finally, add the trees in the list to the transition triggers
+    for unit in trees_in_map_list:
+        if y - 1 <= unit.y < y and y > 0:
+            trigg_winter_tree_transition = source_trigger_manager.add_trigger(
+                name="WinterTreeTransition_x=" + str(unit.x) + "y=" + str(unit.y),
+                enabled=False,
+                looping=False
             )
-            trigg_winter_tree_transition.new_effect.remove_object(
-                source_player=0,
-                area_x1=int(unit.x),
-                area_x2=int(unit.x),
-                area_y1=int(unit.y),
-                area_y2=int(unit.y),
-                object_group=ObjectClass.TREE,
-                object_state=ObjectState.ALIVE
+            # print("WinterTreeTransition_x=" + str(unit.x) + "y=" + str(unit.y))
+            # activate (using last wave)
+            old_trigg_winter_snow_transition_y.new_effect.activate_trigger(
+                trigger_id=trigg_winter_tree_transition.trigger_id)
+            # disable (using current wave)
+            trigg_winter_snow_transition_y.new_effect.deactivate_trigger(
+                trigger_id=trigg_winter_tree_transition.trigger_id)
+            for seasonal_tree in seasonal_trees_list:
+                if unit.rotation == seasonal_tree.SummerVariant:
+                    trigg_winter_tree_transition.new_condition.objects_in_area(
+                        area_x1=int(unit.x),
+                        area_x2=int(unit.x),
+                        area_y1=int(unit.y),
+                        area_y2=int(unit.y),
+                        source_player=0,
+                        quantity=1,
+                        object_group=ObjectClass.TREE,
+                        object_state=ObjectState.ALIVE
+                    )
+                    trigg_winter_tree_transition.new_effect.remove_object(
+                        source_player=0,
+                        area_x1=int(unit.x),
+                        area_x2=int(unit.x),
+                        area_y1=int(unit.y),
+                        area_y2=int(unit.y),
+                        object_group=ObjectClass.TREE,
+                        object_state=ObjectState.ALIVE
+                    )
+                    trigg_winter_tree_transition.new_effect.create_object(
+                        object_list_unit_id=1249,  # Oak Autumn Snow
+                        source_player=0,
+                        facet=int(seasonal_tree.WinterVariant),
+                        location_x=int(unit.x),
+                        location_y=int(unit.y),
+                    )
+    # Finally, add the bushes in the list to the transition triggers
+    for unit in bushes_in_map_list:
+        if y - 1 <= unit.y < y and y > 0:
+            trigg_winter_bush_transition = source_trigger_manager.add_trigger(
+                name="WinterBushTransition_x=" + str(unit.x) + "y=" + str(unit.y),
+                enabled=False,
+                looping=False
             )
-            trigg_winter_tree_transition.new_effect.create_object(
-                object_list_unit_id=1249,
-                source_player=0,
-                facet=int(unit.rotation),
-                location_x=int(unit.x),
-                location_y=int(unit.y),
-            )
-
+            # print("WinterBushTransition_x=" + str(unit.x) + "y=" + str(unit.y))
+            # activate (using last wave)
+            old_trigg_winter_snow_transition_y.new_effect.activate_trigger(
+                trigger_id=trigg_winter_bush_transition.trigger_id)
+            # disable (using current wave)
+            trigg_winter_snow_transition_y.new_effect.deactivate_trigger(
+                trigger_id=trigg_winter_bush_transition.trigger_id)
+            for seasonal_bush in seasonal_bushes_list:
+                if unit.rotation == seasonal_bush.SummerVariant:
+                    trigg_winter_bush_transition.new_condition.objects_in_area(
+                        area_x1=int(unit.x),
+                        area_x2=int(unit.x),
+                        area_y1=int(unit.y),
+                        area_y2=int(unit.y),
+                        source_player=0,
+                        quantity=1,
+                        object_group=ObjectClass.TREE,
+                        object_state=ObjectState.ALIVE
+                    )
+                    trigg_winter_bush_transition.new_effect.remove_object(
+                        source_player=0,
+                        area_x1=int(unit.x),
+                        area_x2=int(unit.x),
+                        area_y1=int(unit.y),
+                        area_y2=int(unit.y),
+                        object_group=ObjectClass.TREE,
+                        object_state=ObjectState.ALIVE
+                    )
+                    trigg_winter_bush_transition.new_effect.create_object(
+                        object_list_unit_id=1054,  # Bush C
+                        source_player=0,
+                        facet=int(seasonal_bush.WinterVariant),
+                        location_x=int(unit.x),
+                        location_y=int(unit.y),
+                    )
+    old_trigg_winter_snow_transition_y = trigg_winter_snow_transition_y
 
 triggerEnd = source_trigger_manager.add_trigger("9===" + identification_name + " End===")
 
--- model/seasonal_trees.py
+++ model/seasonal_trees.py
@@ -3,7 +3,7 @@ class SeasonalTrees:
     Seasonal trees which contains information for each trees
 
     can also includes plants
-
+    Oak -> Oak -> Oak Autumn -> Oak Autumn Snow
 
     """
 
@@ -15,11 +15,23 @@ class SeasonalTrees:
 
     def get_seasonal_trees():
         return [
-            SeasonalTrees(SpringVariant=4, SummerVariant=4, FallVariant=21, WinterVariant=21),
-            SeasonalTrees(SpringVariant=6, SummerVariant=6, FallVariant=19, WinterVariant=19),
+            SeasonalTrees(SpringVariant=4, SummerVariant=4, FallVariant=23, WinterVariant=23),
+            SeasonalTrees(SpringVariant=5, SummerVariant=5, FallVariant=22, WinterVariant=22),
+            SeasonalTrees(SpringVariant=6, SummerVariant=6, FallVariant=24, WinterVariant=24),
             SeasonalTrees(SpringVariant=7, SummerVariant=7, FallVariant=20, WinterVariant=20),
-            SeasonalTrees(SpringVariant=8, SummerVariant=8, FallVariant=18, WinterVariant=18),
-            SeasonalTrees(SpringVariant=27, SummerVariant=27, FallVariant=26, WinterVariant=26),
+            SeasonalTrees(SpringVariant=8, SummerVariant=8, FallVariant=21, WinterVariant=21),
+            SeasonalTrees(SpringVariant=9, SummerVariant=9, FallVariant=19, WinterVariant=19),
+            # 10: dead tree
+            # 11: dead tree
+            # 12: dead tree
+            SeasonalTrees(SpringVariant=13, SummerVariant=13, FallVariant=30, WinterVariant=30),
+            SeasonalTrees(SpringVariant=14, SummerVariant=14, FallVariant=28, WinterVariant=28),
+            SeasonalTrees(SpringVariant=15, SummerVariant=15, FallVariant=29, WinterVariant=29),
+            # 16: dead tree
+            # 17: dead tree
+            # 18: dead tree
+            # 19-24: orange leaf tree
+            # 25-27: dead tree
             SeasonalTrees(SpringVariant=28, SummerVariant=28, FallVariant=26, WinterVariant=26),
             SeasonalTrees(SpringVariant=29, SummerVariant=29, FallVariant=25, WinterVariant=25),
             SeasonalTrees(SpringVariant=30, SummerVariant=30, FallVariant=24, WinterVariant=24),
@@ -28,14 +40,14 @@ class SeasonalTrees:
             SeasonalTrees(SpringVariant=33, SummerVariant=33, FallVariant=0, WinterVariant=0),
             SeasonalTrees(SpringVariant=34, SummerVariant=34, FallVariant=1, WinterVariant=1),
             SeasonalTrees(SpringVariant=35, SummerVariant=35, FallVariant=2, WinterVariant=2),
-            SeasonalTrees(SpringVariant=36, SummerVariant=36, FallVariant=10, WinterVariant=10),
+            SeasonalTrees(SpringVariant=36, SummerVariant=36, FallVariant=3, WinterVariant=3),
             SeasonalTrees(SpringVariant=37, SummerVariant=37, FallVariant=11, WinterVariant=11),
-            SeasonalTrees(SpringVariant=38, SummerVariant=38, FallVariant=40, WinterVariant=40),
-            SeasonalTrees(SpringVariant=39, SummerVariant=39, FallVariant=39, WinterVariant=39),
-
-            SeasonalTrees(SpringVariant=33, SummerVariant=0, FallVariant=0, WinterVariant=0),
-            SeasonalTrees(SpringVariant=33, SummerVariant=1, FallVariant=0, WinterVariant=0),
-            SeasonalTrees(SpringVariant=33, SummerVariant=2, FallVariant=0, WinterVariant=0),
-            SeasonalTrees(SpringVariant=33, SummerVariant=3, FallVariant=0, WinterVariant=0),
-            SeasonalTrees(SpringVariant=33, SummerVariant=5, FallVariant=0, WinterVariant=0),
+            SeasonalTrees(SpringVariant=38, SummerVariant=38, FallVariant=10, WinterVariant=10),
+            SeasonalTrees(SpringVariant=39, SummerVariant=39, FallVariant=41, WinterVariant=41),
+            SeasonalTrees(SpringVariant=40, SummerVariant=40, FallVariant=40, WinterVariant=40),
+            SeasonalTrees(SpringVariant=41, SummerVariant=41, FallVariant=7, WinterVariant=7),
+            SeasonalTrees(SpringVariant=42, SummerVariant=42, FallVariant=12, WinterVariant=12),
+            SeasonalTrees(SpringVariant=1, SummerVariant=1, FallVariant=3, WinterVariant=3),
+            SeasonalTrees(SpringVariant=2, SummerVariant=2, FallVariant=1, WinterVariant=1),
+            SeasonalTrees(SpringVariant=3, SummerVariant=3, FallVariant=2, WinterVariant=2),
         ]
